51b62ef151ff56eac941d2446d2162fd1cabf899
thien ho <thienngoc.x@gmail.com>
2021-06-12 16:48:56 +0700

    update AdminsController

--- AppDev_MCA/Controllers/AdminsController.cs
+++ AppDev_MCA/Controllers/AdminsController.cs
@@ -6,10 +6,24 @@ using System.Web.Mvc;
 
 namespace AppDev_MCA.Controllers
 {
+    [Authorize(Roles = "ADMIN")]
     public class AdminsController : Controller
     {
+
         // GET: Admins
         public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ListTrainingStaff()
+        {
+            return View();
+        }
+        public ActionResult RemoveTrainingStaffAccount()
+        {
+            return View();
+        }
+        public ActionResult CreateTrainingStaff()
         {
             return View();
         }
