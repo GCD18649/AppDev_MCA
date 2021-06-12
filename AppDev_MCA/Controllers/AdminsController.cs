c4edf69dc7d69c6742f3038ab5cb91a5a2c6e8c4
thien ho <thienngoc.x@gmail.com>
2021-06-12 17:02:36 +0700

    create ListTrainingStaff action of AdminsController

--- AppDev_MCA/Controllers/AdminsController.cs
+++ AppDev_MCA/Controllers/AdminsController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AppDev_MCA.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,7 +12,15 @@ namespace AppDev_MCA.Controllers
     [Authorize(Roles = "ADMIN")]
     public class AdminsController : Controller
     {
-
+        private ApplicationDbContext _context;
+        private UserManager<ApplicationUser> _userManager;
+        public AdminsController()
+        {
+            _context = new ApplicationDbContext();
+            _userManager = new UserManager<ApplicationUser>(
+                new UserStore<ApplicationUser>(new ApplicationDbContext())
+            );
+        }
         // GET: Admins
         public ActionResult Index()
         {
@@ -17,7 +28,9 @@ namespace AppDev_MCA.Controllers
         }
         public ActionResult ListTrainingStaff()
         {
-            return View();
+            var roleid = _context.Roles.Where(x => x.Name.Equals("STAFF")).FirstOrDefault().Id;
+            var staffInDb = _context.Users.Where(s => s.Roles.Any(r => r.RoleId == roleid));
+            return View(staffInDb);
         }
         public ActionResult RemoveTrainingStaffAccount()
         {
