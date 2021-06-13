d78bd8b423c69de9265c754d5fbd565e543a6655
thien ho <thienngoc.x@gmail.com>
2021-06-13 21:29:48 +0700

    Create UpdateProfile action

--- AppDev_MCA/Controllers/TrainersController.cs
+++ AppDev_MCA/Controllers/TrainersController.cs
@@ -31,5 +31,29 @@ namespace AppDev_MCA.Controllers
             var TrainerInDb = _context.TrainerUsers.SingleOrDefault(t => t.Id == CurrentTrainerId);
             return View(TrainerInDb);
         }
+        [HttpGet]
+        public ActionResult UpdateProfile()
+        {
+            var CurrentUserId = User.Identity.GetUserId();
+            var UserInDb = _context.TrainerUsers.SingleOrDefault(c => c.Id == CurrentUserId);
+            return View(UserInDb);
+        }
+        [HttpPost]
+        public ActionResult UpdateProfile(TrainerUser trainer)
+        {
+            var CurrentUserId = User.Identity.GetUserId();
+            var UserInDb = _context.Users.SingleOrDefault(c => c.Id == CurrentUserId);
+            var TrainerInDb = _context.TrainerUsers.SingleOrDefault(c => c.Id == CurrentUserId);
+            UserInDb.Email = trainer.EmailAddress;
+            UserInDb.UserName = trainer.EmailAddress;
+            TrainerInDb.FullName = trainer.FullName;
+            TrainerInDb.EmailAddress = trainer.EmailAddress;
+            TrainerInDb.Telephone = trainer.Telephone;
+            TrainerInDb.type = trainer.type;
+            TrainerInDb.WorkingPlace = trainer.WorkingPlace;
+            TrainerInDb.UserName = trainer.EmailAddress;
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
