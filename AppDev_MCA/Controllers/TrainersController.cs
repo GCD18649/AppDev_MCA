0bffcaf5cb4997b7f5c5cc7c551b0723305bdf74
thien ho <thienngoc.x@gmail.com>
2021-06-13 20:56:55 +0700

    Create Index and ViewProfile action of TrainersController

--- AppDev_MCA/Controllers/TrainersController.cs
+++ AppDev_MCA/Controllers/TrainersController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AppDev_MCA.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +11,25 @@ namespace AppDev_MCA.Controllers
 {
     public class TrainersController : Controller
     {
+        private ApplicationDbContext _context;
+        private UserManager<ApplicationUser> _userManager;
+        public TrainersController()
+        {
+            _context = new ApplicationDbContext();
+            _userManager = new UserManager<ApplicationUser>(
+                new UserStore<ApplicationUser>(new ApplicationDbContext())
+            );
+        }
         // GET: Trainers
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult ViewProfile()
+        {
+            var CurrentTrainerId = User.Identity.GetUserId();
+            var TrainerInDb = _context.TrainerUsers.SingleOrDefault(t => t.Id == CurrentTrainerId);
+            return View(TrainerInDb);
         }
     }
 }
